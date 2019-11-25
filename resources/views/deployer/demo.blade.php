34e860bc47e56170dde1830851e9e66e54a67334
quynt-1571 <nguyen.the.quy@sun-asterisk.com>
2019-11-25 17:06:36 +0700

    Hjhj

--- resources/views/deployer/demo.blade.php
+++ resources/views/deployer/demo.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>Hjhj</title>
 </head>
 <body>
 
-<h1>Welcome to Avengers Grpoup</h1>
-<p>Test Auto Deployer</p><br>
+<h1>Welcome to Avengers Grpoup<img src="https://chatpp.thangtd.com/img/emoticons/jp/go.png"></h1>
+<p>Test Auto Deployer</p><img src="https://i.imgur.com/JEWoIdl.png"><br>
 <p>!!!!!!!!!!!!!!!!!!!!!!!!!</p>
 
 </body>
